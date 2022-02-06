b612ea9a8cfb672b1530023d6c0cfd68d5df5b45
Krzysztof-WW <94003069+Krzysztof-WW@users.noreply.github.com>
2022-02-07 00:36:39 +0100

    Add documentation

--- modules/backlight.c
+++ modules/backlight.c
@@ -1,3 +1,13 @@
+/*  brightness module
+    
+    This module shows current brightness of display in laptop.
+
+    Configuration:
+      .str - backlight name
+
+    Backlight names can be acquired with command `ls /sys/class/backlight`
+*/
+
 #include "../kwstatus.h"
 #include <stdio.h>
 #include <sys/inotify.h>
@@ -6,14 +16,14 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define FILEBUF 20
-#define PATHBUF 100
-
-#define BRIGHTNES_DIR "/sys/class/backlight/"
+#define BRIGHTNES_ROOT "/sys/class/backlight/"
 #define BRIGHTNESS "/brightness"
 #define MAX_BRIGHTNESS "/max_brightness"
 
-static char* icons[] = {
+#define FILEBUF 20
+#define PATHBUF 100
+
+static const char* icons[] = {
   "🌑",
   "🌒",
   "🌓",
@@ -25,10 +35,10 @@ void
 backlight(void* self) {
   struct modules* mod = (struct modules*)self;
   int bc_watch, bc_read;
-  char file[PATHBUF+1] = BRIGHTNES_DIR;
+  char file[PATHBUF+1] = BRIGHTNES_ROOT;
   int file_end;
   char bcstr[FILEBUF+1] = {0};
-  char out[FILEBUF+1];
+  char out[MODSIZE];
   unsigned int backlight, max_brightness;
   short icon;
   struct inotify_event event;
@@ -37,13 +47,14 @@ backlight(void* self) {
   strncat(file, mod->str, PATHBUF);
   file_end = strlen(file);
 
-  /* read max_backlight */
+  /* open max_backlight */
   strncat(file, MAX_BRIGHTNESS, PATHBUF);
   bc_read = open(file, O_RDONLY);
   if(bc_read == -1) {
     fputs("backlight: cannot open max_brightness file\n", stderr);
     return;
   }
+  /* read max_backlight */
   read(bc_read, bcstr, FILEBUF);
   max_brightness = atoi(bcstr);
   close(bc_read);
@@ -57,6 +68,7 @@ backlight(void* self) {
     return;
   }
 
+  /* setup inotify */
   bc_watch = inotify_init();
   inotify_add_watch(bc_watch, file, IN_MODIFY);
 
@@ -65,7 +77,7 @@ backlight(void* self) {
     lseek(bc_read, 0, SEEK_SET);
     bcstr[read(bc_read, bcstr, FILEBUF)] = 0;
 
-    /* text */
+    /* generate text */
     backlight = atoi(bcstr);
     backlight = backlight*100/max_brightness;
     if(backlight > 80)
@@ -79,11 +91,10 @@ backlight(void* self) {
     else
       icon = 4;
 
-    snprintf(out, FILEBUF, "%s %d%%", icons[icon], backlight);
-
+    snprintf(out, MODSIZE - 1, "%s %d%%", icons[icon], backlight);
     mod_update(mod, out);
 
-    /* wait for modification */
+    /* wait for modifications */
     read(bc_watch, &event, sizeof(struct inotify_event));
   }
 }
